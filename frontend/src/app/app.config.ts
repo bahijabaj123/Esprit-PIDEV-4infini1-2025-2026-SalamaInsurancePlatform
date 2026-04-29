34ac19e20d90812a3820b13d5c95e799a585f641
ramiatig <Rami.benatig@esprit.tn>
2026-04-29 20:01:18 +0100

    feat: ajout frontend Rami

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,17 +1,6 @@
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
-
-import { authJwtInterceptor } from './core/auth/auth-jwt.interceptor';
-import { routes } from './app.routes';
+import { ApplicationConfig } from '@angular/core';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
-    provideAnimations(),
-    provideHttpClient(withInterceptors([authJwtInterceptor]))
-  ]
+  providers: [provideHttpClient(withFetch())]
 };
-
